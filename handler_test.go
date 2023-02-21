87e9bb89403e3e22e95e7eafffd85cf22041f49e
Mochammad Qemal Firza <kemalfirza@gmail.com>
2023-02-21 12:43:50 +0700

    servemux

--- handler_test.go
+++ handler_test.go
@@ -21,3 +21,23 @@ func TestHandler(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Welkam Tu Mai Cenel")
+	})
+	mux.HandleFunc("/saskrep", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "plis saskrep")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8084",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
